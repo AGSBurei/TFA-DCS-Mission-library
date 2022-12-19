f6373c05e32796d93a78d33e0a19c36d871ccc50
Elisia88 <82525026+Elisia88@users.noreply.github.com>
2022-12-19 16:48:28 +0100

    IADS BLUE/RED

--- IADS/redIadsInit.lua
+++ IADS/redIadsInit.lua
@@ -1,38 +1,26 @@
 -- setup red IADS
-blueIADS = SkynetIADS:create('IADS South')
-blueIADS:addSAMSitesByPrefix('SAM')
-blueIADS:addEarlyWarningRadarsByPrefix('EW')
-blueIADS:activate()
-blueIADS:addRadioMenu()
+redIADSminhad = SkynetIADS:create('IADS Minhad')
+redIADSminhad:addSAMSitesByPrefix('SAMM')
+redIADSminhad:addEarlyWarningRadarsByPrefix('EWM')
+redIADSminhad:activate()
+redIADSminhad:addRadioMenu()
 
-
---debug red IADS
-local iadsDebug = blueIADS:getDebugSettings()
-iadsDebug.IADSStatus = true
-iadsDebug.radarWentDark = true
-iadsDebug.contacts = true
-iadsDebug.radarWentLive = true
-iadsDebug.noWorkingCommmandCenter = true
-iadsDebug.samNoConnection = true
-iadsDebug.jammerProbability = true
-iadsDebug.addedEWRadar = true
-iadsDebug.harmDefence = true
-iadsDebug.samSiteStatusEnvOutput = true
-iadsDebug.earlyWarningRadarStatusEnvOutput = true
-iadsDebug.commandCenterStatusEnvOutput = true
 
 --RED SAM LIST
-blueIADS:getSAMSitesByPrefix('SAM-SA-2'):setGoLiveRangeInPercent(70):setHARMDetectionChance(50)
-blueIADS:getSAMSitesByPrefix('SAM-SA-20A'):setGoLiveRangeInPercent(65):setHARMDetectionChance(90)
-blueIADS:getSAMSitesByPrefix('SAM-SA-9'):setGoLiveRangeInPercent(100):setHARMDetectionChance(50)
-blueIADS:getSAMSitesByPrefix('SAM-SA-8'):setGoLiveRangeInPercent(100):setHARMDetectionChance(75)
+redIADSminhad:getSAMSitesByPrefix('SAMM-SA-6'):setGoLiveRangeInPercent(70):setHARMDetectionChance(30)
+redIADSminhad:getSAMSitesByPrefix('SAMM-SA-15'):setGoLiveRangeInPercent(80):setHARMDetectionChance(100)
+redIADSminhad:getSAMSitesByPrefix('SAMM-SA-8'):setGoLiveRangeInPercent(100):setHARMDetectionChance(50)
 
 --declaration of point defence by GroupName
-local SA15PD = blueIADS:getSAMSiteByGroupName('SAM-SA-15-PD-SA-20A-RED-SOUTH')
+local SA15PD = redIADSminhad:getSAMSiteByGroupName('SAMM-SA-15PD')
+local SA15PD1 = redIADSminhad:getSAMSiteByGroupName('SAMM-SA-15PD-1')
+local SA15PD2 = redIADSminhad:getSAMSiteByGroupName('SAMM-SA-15PD-2')
 --set point defence to SAMSiteGroupe by name
-blueIADS:getSAMSiteByGroupName('SAM-SA-20A-RED-SOUTH'):addPointDefence(SA15PD):setHARMDetectionChance(100)
+redIADSminhad:getSAMSiteByGroupName('SAMM-SA-6'):addPointDefence(SA15PD):setHARMDetectionChance(100)
+redIADSminhad:getSAMSiteByGroupName('SAMM-SA-6-1'):addPointDefence(SA15PD1):setHARMDetectionChance(100)
+redIADSminhad:getSAMSiteByGroupName('SAMM-SA-6-2'):addPointDefence(SA15PD2):setHARMDetectionChance(100)
 
-local commandCenter = StaticObject.getByName("Static ERO HQ Bunker")
-blueIADS:addCommandCenter(commandCenter)
+local commandCenter = StaticObject.getByName("ERO HQ Bunker-Minhad")
+redIADSminhad:addCommandCenter(commandCenter)
 
 
