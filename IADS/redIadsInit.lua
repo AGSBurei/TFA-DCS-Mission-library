d963a6f7fabe924e0e1aa75fc7aaa96dac0b3ab9
Elisia88 <82525026+Elisia88@users.noreply.github.com>
2022-12-19 17:53:34 +0100

    Added PD EWR

--- IADS/redIadsInit.lua
+++ IADS/redIadsInit.lua
@@ -29,10 +29,12 @@ redIADSminhad:getSAMSitesByPrefix('SAMM-SA-8'):setGoLiveRangeInPercent(100):setH
 local SA15PD = redIADSminhad:getSAMSiteByGroupName('SAMM-SA-15PD')
 local SA15PD1 = redIADSminhad:getSAMSiteByGroupName('SAMM-SA-15PD-1')
 local SA15PD2 = redIADSminhad:getSAMSiteByGroupName('SAMM-SA-15PD-2')
+local SA15PDEWR = redIADSminhad:getSAMSiteByGroupName('SAMM-SA-15PD-EWR')
 --set point defence to SAMSiteGroupe by name
 redIADSminhad:getSAMSiteByGroupName('SAMM-SA-6'):addPointDefence(SA15PD):setHARMDetectionChance(100)
 redIADSminhad:getSAMSiteByGroupName('SAMM-SA-6-1'):addPointDefence(SA15PD1):setHARMDetectionChance(100)
 redIADSminhad:getSAMSiteByGroupName('SAMM-SA-6-2'):addPointDefence(SA15PD2):setHARMDetectionChance(100)
+redIADSminhad:getSAMSiteByGroupName('EWM'):addPointDefence(SA15PDEWR):setHARMDetectionChance(100)
 
 local commandCenter = StaticObject.getByName("ERO HQ Bunker-Minhad")
 redIADSminhad:addCommandCenter(commandCenter)
