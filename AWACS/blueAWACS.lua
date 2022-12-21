9cec17856dfa8705141af7e7147f9031054bb28a
Elisia88 <82525026+Elisia88@users.noreply.github.com>
2022-12-21 11:12:42 +0100

    Update blueAWACS.lua

--- AWACS/blueAWACS.lua
+++ AWACS/blueAWACS.lua
@@ -1,5 +1,9 @@
 AWACS = UNIT:FindByName( "EWB-E3" )
 
+Spawn_AWACS = SPAWN:New( AWACS )
+Spawn_Plane:SpawnAtAirbase( AIRBASE:FindByName( "AIRBASE.PersianGulf.Liwa_Airbase" ), SPAWN.Takeoff.Cold )
+
+
  function AWACS:OnEventDead( EventData )
    Spawn_AWACS = SPAWN:New( AWACS )
    Spawn_Plane:SpawnAtAirbase( AIRBASE:FindByName( "AIRBASE.PersianGulf.Liwa_Airbase" ), SPAWN.Takeoff.Cold )
